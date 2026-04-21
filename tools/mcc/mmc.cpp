827da8ba57da49e3afdeafceaa21d5bd1047c4ca
meonnmi-ops <meonnmi-ops@users.noreply.github.com>
2026-04-22 00:27:09 +0630

    Fix: Update LLVM TargetRegistry header path for LLVM 18

--- tools/mcc/mmc.cpp
+++ tools/mcc/mmc.cpp
@@ -8,7 +8,7 @@
 #include <llvm/IR/LegacyPassManager.h>
 #include <llvm/IR/Verifier.h>
 #include <llvm/Support/TargetSelect.h>
-#include <llvm/Support/TargetRegistry.h>
+#include <llvm/MC/TargetRegistry.h>
 #include <llvm/Support/raw_ostream.h>
 #include <llvm/Support/FileSystem.h>
 #include <llvm/Target/TargetMachine.h>
